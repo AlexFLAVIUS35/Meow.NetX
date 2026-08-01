3c22df71cf85ef45d9edfad5a101c9ee5cc54106
Flavius Alex <lazarflavius29@gmail.com>
2026-08-02 00:01:50 +0300

    Auto sync 2026-08-02 00:01:50

--- DevPresence/Program.cs
+++ DevPresence/Program.cs
@@ -1,19 +1,16 @@
 using DiscordRPC;
 
-var rpc = new DiscordRpcClient("YOUR_DISCORD_APPLICATION_ID");
+var rpc = new DiscordRpcClient("1533218071148495038");
 
 rpc.Initialize();
 
 rpc.SetPresence(new RichPresence
 {
     Details = "Working on Meow.NetX",
-    State = "Editing code",
-    Assets = new Assets
-    {
-        LargeImageKey = "meownet",
-        LargeImageText = "Meow.NetX"
-    }
+    State = "Editing code"
 });
 
 Console.WriteLine("Discord Rich Presence running.");
+Console.WriteLine("Press ENTER to exit.");
+
 Console.ReadLine();
